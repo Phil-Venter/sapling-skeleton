6c79fb0914dc84a45a3b11176db8d5c1eaf5bfab
Philip <mrphilventer@gmail.com>
2026-02-18 17:40:47 +0200

    eh

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
-$router = initialise(".env");
 
+register_exception_handlers();
+load_env(from_base_dir(".env"));
+require_once from_base_dir("bootstrap.php");
+
+$router = new Sapling\Core\Router(Sapling\Core\Request::fromGlobals());
 require_once from_base_dir("routes.php");
-
 if (!$router->matched) {
     Sapling\Core\Response::notFound("Not found")->send();
 }
--- routes.php
+++ routes.php
@@ -3,6 +3,17 @@
 /** @var Sapling\Core\Router $router */
 use Sapling\Core\Response;
 
-$router->get("ping", static fn() => Response::ok("pong", [
-    "Content-Type" => "text/plain; charset=utf-8",
-]));
+$router->get("ping", function () {
+    pdo()->prepare(<<<'SQL'
+        INSERT INTO visits (ip, "count", date)
+        VALUES (:ip, 1, :date)
+        ON CONFLICT(ip, date) DO UPDATE SET
+        "count" = "count" + 1;
+    SQL)
+    ->execute([
+        "ip" => $_SERVER["REMOTE_ADDR"],
+        "date" => date("Y-m-d"),
+    ]);
+
+    return Response::ok("pong", ["Content-Type" => "text/plain; charset=utf-8"]);
+});
